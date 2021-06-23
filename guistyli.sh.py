baed742a4df77e4afae4953d0e673fc629ba96d8
AlmirPaulo <ap.freelas@gmail.com>
2021-06-22 22:43:13 -0300

    update

--- guistyli.sh.py
+++ guistyli.sh.py
@@ -1,24 +1,28 @@
 from os import system 
+#from time import sleep
 import PySimpleGUI as sg
+#import threading
 
-
+        
 #Window
-def window():
-    sg.theme('DarkBlue8')
-    layout = [
-            [sg.Text('Window Manager: '),sg.Combo(['KDE','GNOME', 'XFCE', 'Sway', 'feh', 'nitrogen'], key='wm') ],
-            [sg.Text('Theme: '), sg.Input(key='theme')],
-            [sg.Text('Change the wallpaper hourly?'), sg.Checkbox('', key='always_change')],
-            [sg.Button('Go!')]
-            ]
-    
-    #Create Window
-    return sg.Window('GUIStyli.sh', layout, finalize=True)
+
+sg.theme('DarkBlue8')
+layout = [
+        [sg.Text('Window Manager: '),sg.Combo(['KDE','GNOME', 'XFCE', 'Sway', 'feh', 'nitrogen'], key='wm') ],
+        [sg.Text('Theme: '), sg.Input(key='theme')],
+#        [sg.Text('Change the wallpaper hourly?'), sg.Checkbox('', key='always_change')], 
+        [sg.Button('Change')]
+        ]
+
+#Create Window
+window = sg.Window('GUIStyli.sh', layout)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
-    ev, val = window().read()
-    if ev == 'Go!':
+    ev, val = window.read()
+    if ev == sg.WIN_CLOSED: 
+        break
+    if ev == 'Change':
         #Handle the WM
         if val['wm'] == 'feh':
             wm = ''
@@ -38,16 +42,23 @@ while True:
         if val['theme'] == '':
             theme = ''
         final_string = f"./styli.sh {wm} {theme}" 
+        system(final_string)
         
-        if val['always_change'] == False:
-            system(final_string)
+        #if val['always_change'] == False:
+        #   system(final_string)
         
-        else:
-            #Keep changing wallpaper hourly
-            set_interval = f"while [ 1 > 0 ]; do {final_string} && sleep 5; done  " 
-            system(set_interval)
-        window().hide()
-    if ev == sg.WIN_CLOSED: 
-        break
+        #else:
+        #    #Keep changing wallpaper hourly
 
+        #    new_layout = [
+        #            [sg.Text('Stop the change of wallpapers?')], 
+        #            [sg.Button('Stop!')]
+        #            ] 
 
+        #    window.close()
+        #    new_window = sg.Window('GUIStyli.sh', new_layout)
+        #    ev2, val2 = new_window.read(timeout=1000)
+
+        #    if ev2 == 'Stop!':
+        #        break
+          
